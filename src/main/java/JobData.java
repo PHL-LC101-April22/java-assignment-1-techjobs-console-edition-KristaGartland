1a36f894a54febe2cc2df6a63c0922958b231594
Krista Gartland <Randomflyingobject@yahoo.com>
2022-07-28 20:04:02 -0500

    First try at find by value

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -98,8 +95,22 @@ public class JobData {
         // load data, if not already loaded
         loadData();
 
-        // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> person : allJobs){
+
+           for (Map.Entry<String, String> category : person.entrySet()){
+              String searchCategory =  category.getValue();
+               if (jobs.contains(person)) {
+                continue;
+               } else if (searchCategory.contains(value)){
+                   jobs.add(person);
+
+               }
+
+           }
+        }
+        return jobs;
     }
 
     /**
